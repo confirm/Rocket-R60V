bb08ddceee8428079fcef9f9754926fd295320ab
Dominique Barton <dbarton@confirm.ch>
2019-12-24 03:03:31 +0100

    FIX: Activate settings

--- rocket/settings/__init__.py
+++ rocket/settings/__init__.py
@@ -4,3 +4,6 @@ Settings module for the Rocket R 60V.
 
 from .temperature_unit import TemperatureUnit
 from .language import Language
+from .brew_boiler_temperature import BrewBoilerTemperature
+from .service_boiler_temperature import ServiceBoilerTemperature
+from .water_feed import WaterFeed
